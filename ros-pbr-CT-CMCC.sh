3ea0bcc82716793e597f3ed8a8cc4c2af95f2ac7
jacyl4 <jacyl4@gmail.com>
2020-03-08 23:08:52 +0800

    auto

--- ros-pbr-CT-CMCC.sh
+++ ros-pbr-CT-CMCC.sh
@@ -3,19 +3,17 @@
 mkdir -p /tmp/pbr
 
 #电信
-curl https://bgp.space/chinatelecom.html > /tmp/pbr/ct.txt && sed -i '1,/BEGIN/d' /tmp/pbr/ct.txt && sed -i '/END/,$d' /tmp/pbr/ct.txt && sed -i 's/<br>//g' /tmp/pbr/ct.txt
+curl https://bgp.space/chinanet.html > /tmp/pbr/ct.txt && sed -i '1,/BEGIN/d' /tmp/pbr/ct.txt && sed -i '/END/,$d' /tmp/pbr/ct.txt && sed -i 's/<br>//g' /tmp/pbr/ct.txt
 #联通
-curl https://bgp.space/unicom_cnc.html > /tmp/pbr/cnc.txt && sed -i '1,/BEGIN/d' /tmp/pbr/cnc.txt && sed -i '/END/,$d' /tmp/pbr/cnc.txt && sed -i 's/<br>//g' /tmp/pbr/cnc.txt
+curl https://bgp.space/unicom.html > /tmp/pbr/cnc.txt && sed -i '1,/BEGIN/d' /tmp/pbr/cnc.txt && sed -i '/END/,$d' /tmp/pbr/cnc.txt && sed -i 's/<br>//g' /tmp/pbr/cnc.txt
 #移动
 curl https://bgp.space/cmcc.html > /tmp/pbr/cmcc.txt && sed -i '1,/BEGIN/d' /tmp/pbr/cmcc.txt && sed -i '/END/,$d' /tmp/pbr/cmcc.txt && sed -i 's/<br>//g' /tmp/pbr/cmcc.txt
 #铁通
-curl https://bgp.space/crtc.html > /tmp/pbr/crtc.txt && sed -i '1,/BEGIN/d' /tmp/pbr/crtc.txt && sed -i '/END/,$d' /tmp/pbr/crtc.txt && sed -i 's/<br>//g' /tmp/pbr/crtc.txt
+curl https://bgp.space/tietong.html > /tmp/pbr/crtc.txt && sed -i '1,/BEGIN/d' /tmp/pbr/crtc.txt && sed -i '/END/,$d' /tmp/pbr/crtc.txt && sed -i 's/<br>//g' /tmp/pbr/crtc.txt
 #教育网
 curl https://bgp.space/cernet.html > /tmp/pbr/cernet.txt && sed -i '1,/BEGIN/d' /tmp/pbr/cernet.txt && sed -i '/END/,$d' /tmp/pbr/cernet.txt && sed -i 's/<br>//g' /tmp/pbr/cernet.txt
-#长城宽带/鹏博士
-curl https://bgp.space/gwbn.html > /tmp/pbr/gwbn.txt && sed -i '1,/BEGIN/d' /tmp/pbr/gwbn.txt && sed -i '/END/,$d' /tmp/pbr/gwbn.txt && sed -i 's/<br>//g' /tmp/pbr/gwbn.txt
-#国内其他
-curl https://bgp.space/othernet.html > /tmp/pbr/othernet.txt && sed -i '1,/BEGIN/d' /tmp/pbr/othernet.txt && sed -i '/END/,$d' /tmp/pbr/othernet.txt && sed -i 's/<br>//g' /tmp/pbr/othernet.txt
+#科技网
+curl https://bgp.space/cstnet.html > /tmp/pbr/cstnet.txt && sed -i '1,/BEGIN/d' /tmp/pbr/cstnet.txt && sed -i '/END/,$d' /tmp/pbr/cstnet.txt && sed -i 's/<br>//g' /tmp/pbr/cstnet.txt
 
 {
 echo "/ip route rule"
@@ -45,12 +43,7 @@ for net in $nets ; do
   echo "add dst-address=$net action=lookup table=CT"
 done
 
-nets=`cat /tmp/pbr/gwbn.txt`
-for net in $nets ; do
-  echo "add dst-address=$net action=lookup table=CT"
-done
-
-nets=`cat /tmp/pbr/othernet.txt`
+nets=`cat /tmp/pbr/cstnet.txt`
 for net in $nets ; do
   echo "add dst-address=$net action=lookup table=CT"
 done
@@ -85,12 +78,7 @@ for net in $nets ; do
   echo "add list=dpbr-CT address=$net"
 done
 
-nets=`cat /tmp/pbr/gwbn.txt`
-for net in $nets ; do
-  echo "add list=dpbr-CT address=$net"
-done
-
-nets=`cat /tmp/pbr/othernet.txt`
+nets=`cat /tmp/pbr/cstnet.txt`
 for net in $nets ; do
   echo "add list=dpbr-CT address=$net"
 done
